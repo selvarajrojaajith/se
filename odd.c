e9e9955500a45de7d187dddcff21a5f29fb53823
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-05 16:48:30 +0530

    odd.c
    odd or even

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+{
+printf("\n enter the odd number");
+scanf("%d",&odd);
+print("\n enter the even number");
+scanf("%d",&even);
+}
+else
+getch();
+}
